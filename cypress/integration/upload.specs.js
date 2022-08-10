8a7f892967cf80957cfc6821bb40f8e3fe68bfaa
Adrianoes <32403800+Adrianoes@users.noreply.github.com>
2022-08-10 01:01:07 -0400

    training-wheels-cypress-universe

--- cypress/integration/upload.specs.js
+++ cypress/integration/upload.specs.js
@@ -9,4 +9,5 @@ it('deve fazer o upload da minha foto', function(){
     cy.get('input[value=Upload]').click()
     cy.get('img[src="/uploads/image.jpg"]', {timeout: 7000})
     .should('be.visible')
+    
 })
